3e4b28c0100586f336a9191e6ec1c6d9f0b1caf7
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-05-15 15:42:53 -0300

    luz spot

--- src/script.js
+++ src/script.js
@@ -51,6 +51,12 @@ rectAreaLight.position.set(-1.5, 0, 1.5)
 rectAreaLight.lookAt(new THREE.Vector3())
 scene.add(rectAreaLight)
 
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+spotLight.target.position.x = -0.75
+scene.add(spotLight.target)
+scene.add(spotLight)
+
 /**
  * Objects
  */
