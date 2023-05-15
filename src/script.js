2528a9fdb2df58b442ff53f073d00b3216ca23a9
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-05-15 17:08:24 -0300

    alterações na luz

--- src/script.js
+++ src/script.js
@@ -42,7 +42,7 @@ scene.add(directionalLight)
 const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
 scene.add(hemisphereLight)
 
-const pointLight = new THREE.PointLight(0xff9000, 0.5)
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
 pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
 
@@ -56,6 +56,7 @@ scene.add(rectAreaLight)
  */
 // Material
 const material = new THREE.MeshStandardMaterial()
+//material.side = THREE.DoubleSide
 material.roughness = 0.4
 
 // Objects
